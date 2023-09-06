5509a5026624db446fc59e145efe4ae1cd968ed2
suby-kwak <rhkdbtj@gmail.com>
2023-09-06 23:40:20 +0900

    Feat: 4

---
+++ src/Main4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        while (A < 0) {
+            System.out.println("A를 다시 입력하시오");
+            A = scanner.nextInt();
+        }
+
+        int B = scanner.nextInt();
+        while (B > 10) {
+            System.out.println("B를 다시 입력하시오");
+            B = scanner.nextInt();
+        }
+
+        System.out.println(A * B);
+    }
+}
+

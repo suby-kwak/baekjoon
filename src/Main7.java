e8a409e9f6d0c9e90ba0bf56a3239b474daa27c0
suby-kwak <rhkdbtj@gmail.com>
2023-09-09 12:23:39 +0900

    Feat: 7

---
+++ src/Main7.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String id = scanner.next();
+
+        System.out.println(id + "??!");
+    }
+}

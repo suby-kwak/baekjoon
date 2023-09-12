90180a4742a4d68e387ee57fabb47a2653b7e773
suby-kwak <rhkdbtj@gmail.com>
2023-09-12 21:40:34 +0900

    Feat: 10

---
+++ src/Main10.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        while (A < 100 || A > 999) {
+            A = scanner.nextInt();
+        }
+
+        int B = scanner.nextInt();
+        while (B < 100 || B > 999) {
+            B = scanner.nextInt();
+        }
+
+        System.out.println(A*(B%10));
+        System.out.println(A*(B/10%10));
+        System.out.println(A*(B/100));
+        System.out.println(A*B);
+    }
+}

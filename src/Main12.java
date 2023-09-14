93618ea1774f7817dd2107943a00a64b263046f9
suby-kwak <rhkdbtj@gmail.com>
2023-09-14 20:05:44 +0900

    Feat: 12

---
+++ src/Main12.java
@@ -0,0 +1,8 @@
+public class Main12 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}

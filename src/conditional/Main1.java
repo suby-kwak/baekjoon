d7158c5093a3d1e35ae5507fef089877429fc686
suby-kwak <rhkdbtj@gmail.com>
2023-09-16 18:49:37 +0900

    Feat: Conditional 1

---
+++ src/conditional/Main1.java
@@ -0,0 +1,31 @@
+package conditional;
+
+import java.util.Scanner;
+
+public class Main1 {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int A = scanner.nextInt();
+        check(A);
+        int B = scanner.nextInt();
+        check(B);
+
+        if (A > B) {
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+
+    }
+
+    public static int check(int num) {
+        if (num < -10000 || num > 100000) {
+            num = scanner.nextInt();
+        }
+
+        return num;
+    }
+}

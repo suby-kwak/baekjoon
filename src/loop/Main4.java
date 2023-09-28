794086af58383bfe980c7b360c813fe23f2c2366
DESKTOP-L05AT10\곽유섭 <rhkdbtj@gmail.com>
2023-09-28 22:39:34 +0900

    Feat: Loop 4

---
+++ src/loop/Main4.java
@@ -0,0 +1,46 @@
+package loop;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main4 {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int X = scanner.nextInt();
+        check(X, 1000000000);
+
+        int N = scanner.nextInt();
+        check(N, 100);
+
+        List<Integer> aList = new ArrayList<>();
+
+        int result = 0;
+
+        for (int i = 0; i < N; i++) {
+            int a = scanner.nextInt();
+            check(a, 1000000);
+            int b = scanner.nextInt();
+            check(b, 10);
+            aList.add(a * b);
+        }
+
+        for (int i = 0; i < aList.size(); i++) {
+            result += aList.get(i);
+        }
+
+        if (X == result) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+    }
+
+    public static void check(int num, int limit) {
+        while (num < 1 || num > limit) {
+            num = scanner.nextInt();
+        }
+    }
+}

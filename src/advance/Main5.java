da4880f054010afc3f0736ba9919599f9ca9c5a9
suby-kwak <rhkdbtj@gmail.com>
2023-11-05 15:03:05 +0900

    Feat: Advance 5

---
+++ src/advance/Main5.java
@@ -0,0 +1,37 @@
+package advance;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = reader.readLine().toUpperCase();
+        List<String> s = Arrays.asList(str.split(""));
+
+        Map<String, Long> collect = s.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        long check = 0;
+        String result = null;
+
+        for (String key : collect.keySet()) {
+            if (collect.get(key) > check) {
+                check = collect.get(key);
+                result = key;
+            } else if (collect.get(key) == check) {
+                result = "?";
+            }
+        }
+
+        writer.write(result + "\n");
+        writer.close();
+        reader.close();
+
+    }
+}
